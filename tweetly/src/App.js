75ca8f223522fbac75ddfd8b63de57a2435754e9
Abhishek <pundirabhishek987@gmail.com>
2023-06-14 23:14:24 +0530

    refactor: code

--- tweetly/src/App.js
+++ tweetly/src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { Landing } from './components/Landing/Landing';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Tweetly</h1>
-      <Landing />
     </div>
   );
 }
