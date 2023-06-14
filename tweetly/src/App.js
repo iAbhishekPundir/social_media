e174388b72cabf73d9567d8cd4843830d3980c3e
Abhishek <pundirabhishek987@gmail.com>
2023-06-14 23:40:17 +0530

    feature: Login Component

--- tweetly/src/App.js
+++ tweetly/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import { Login } from "./components/Login/Login";
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Tweetly</h1>
+      <Login />
     </div>
   );
 }
