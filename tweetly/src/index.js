9e25dd36f4d22224dc3f4be9a667dc3f87dd1cbe
Abhishek <pundirabhishek987@gmail.com>
2023-06-14 07:41:01 +0530

    feat: add server to the app

--- tweetly/src/index.js
+++ tweetly/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
