5d1a267cdbc493376f2117a988e0d0382067a257
Abhishek <pundirabhishek987@gmail.com>
2023-06-16 17:50:22 +0530

    feature: NavBar

---
+++ tweetly/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,81 @@
+import { Twitter } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "20%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography
+          variant="h6"
+          sx={{ display: { xs: "none", sm: "block", fontFamily: "cursive" } }}
+        >
+          Tweetly
+        </Typography>
+        <Twitter sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search people..." />
+        </Search>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
