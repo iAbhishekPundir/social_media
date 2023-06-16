13b7fb3183579378c345adc149ffc9dd3e6a3734
Abhishek <pundirabhishek987@gmail.com>
2023-06-16 17:48:15 +0530

    feature: Routing

--- tweetly/src/App.js
+++ tweetly/src/App.js
@@ -1,10 +1,42 @@
-import "./App.css";
-import { Login } from "./components/Login/Login";
+// import "./App.css";
+// import "./utils.css";
+import { useState } from "react";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+// import { Login } from "./Features/Login/Login";
+import UserFeed from "./components/UserFeed/UserFeed";
+import Rightbar from "./components/Rightbar/Rightbar";
+import AddPost from "./components/AddPost/AddPost";
+import Navbar from "./components/NavBar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import SignIn from "./Features/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import { Explore } from "./Features/Explore/Explore";
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        {/* <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <UserFeed />
+          <Rightbar />
+        </Stack>
+        <AddPost /> */}
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/explore" element={<Explore/>}/>
+        </Routes>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- tweetly/src/index.js
+++ tweetly/src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import { makeServer } from "./server";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { AuthContextProvider } from "./Contexts/Auth/AuthContext";
 // Call make Server
 makeServer();
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      {/* <AuthContextProvider> */}
+        <App />
+      {/* </AuthContextProvider> */}
+    </Router>
   </React.StrictMode>
 );
