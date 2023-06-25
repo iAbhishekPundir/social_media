bbc482268e2ad64b1e1e8794a0997d9b4aa1d839
Abhishek <pundirabhishek987@gmail.com>
2023-06-25 21:25:13 +0530

    feat: DataContext & DataServices

--- tweetly/src/index.js
+++ tweetly/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { makeServer } from "./server";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { makeServer } from "./server";
 import { AuthContextProvider } from "./Contexts/Auth/AuthContext";
+import { DataContextProvider } from "./Contexts/Data/DataContext";
 // Call make Server
 makeServer();
 
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <Router>
       <AuthContextProvider>
+        <DataContextProvider>
         <App />
+        </DataContextProvider>
       </AuthContextProvider>
     </Router>
   </React.StrictMode>
