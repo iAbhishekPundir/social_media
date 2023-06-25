5e3cf80c809de717a7718c64ab99bbcfadf843a3
Abhishek <pundirabhishek987@gmail.com>
2023-06-25 21:24:35 +0530

    feat: toast for alerts

--- tweetly/src/App.js
+++ tweetly/src/App.js
@@ -2,17 +2,22 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import UserFeed from "./components/UserFeed/UserFeed";
-
 import SignIn from "./Features/SignIn/SignIn";
 import SignUp from "./Features/SignUp/SignUp";
 import { Explore } from "./Features/Explore/Explore";
 import { Bookmark } from "./Features/Bookmark/Bookmark";
 import { Profile } from "./Features/Profile/Profile";
 import { MainContainer } from "./components/MainContainer/MainContainer";
-
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { useData } from "./Contexts/Data/DataContext";
 function App() {
-
+  const {isLoading} = useData();
   return (
+    <div >
+      <ToastContainer position="top-center" autoClose={1100}
+      reverseOrder={false}/>
+      {isLoading && <h2>Loading...</h2>}
     <Routes>
       <Route
         path="/"
@@ -59,6 +64,7 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       {/* <Route path="/mockman" element={<Mockman />} /> */}
     </Routes>
+    </div>
   );
 }
 
