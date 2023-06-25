d0c245b083d1573a615bc8b568488869afe2aec9
Abhishek <pundirabhishek987@gmail.com>
2023-06-25 21:25:36 +0530

    feat: Auth Service

--- tweetly/src/Auth/RequireAuth.js
+++ tweetly/src/Auth/RequireAuth.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { AuthState } from "../Contexts/Auth/AuthContext";
+import { useAuth } from "../Contexts/Auth/AuthContext";
 
 export const RequiresAuth = ({ children }) => {
-  const { isLoggedIn } = AuthState();
+  const { isLoggedIn } = useAuth();
   const location = useLocation();
 
   return isLoggedIn ? (
