36490fb16336eb5d5aa0c903a8b48dd30b02f069
Abhishek <pundirabhishek987@gmail.com>
2023-06-28 23:10:23 +0530

    feat: Bookmark

---
+++ tweetly/src/Features/Bookmark/Bookmark.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useData } from '../../Contexts/Data/DataContext'
+import { SinglePost } from '../../components/SinglePost/SinglePost'
+import "./Bookmark.css"
+import { useEffect } from 'react'
+export const Bookmark = () => {
+
+const {dataState:{users,posts},setIsLoading}=useData()
+const socialUser=JSON.parse(localStorage.getItem("socialUser"))
+
+const loggedInUser=users?.find(({username})=>username===socialUser.username) 
+const newBookmark=posts?.filter(({_id})=>loggedInUser?.bookmarks.includes(_id))
+
+
+useEffect(()=>{
+  window.scrollTo(0, 0)
+  setIsLoading(true)
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 300)
+},[])
+  return (
+    <div>
+    {newBookmark?.length > 0 ? (
+      newBookmark.map((data) => <SinglePost data={data} key={data._id} />)
+    ) : (
+      <p className='bookmarke-heading'>No bookmarks available.</p>
+    )}
+  </div>
+  )
+}
